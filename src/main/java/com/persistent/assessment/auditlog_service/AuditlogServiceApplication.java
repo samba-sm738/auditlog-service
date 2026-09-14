854a2837850226bbcab0327a46590bb361216015
coreprism-ai <samba.sm738@gmail.com>
2026-09-14 11:55:45 -0700

    initial code

---
+++ src/main/java/com/persistent/assessment/auditlog_service/AuditlogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.persistent.assessment.auditlog_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditlogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuditlogServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/persistent/assessment/auditlog_service/AuditlogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.persistent.assessment.auditlog_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditlogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
